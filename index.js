0a07ab7ccce3a5b08089c3f6c4ef183fc347856a
Leo-ground <68109622+Leo-ground@users.noreply.github.com>
2020-09-02 16:31:49 +0900

    #2.2

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-/*function sayHello(name, age) {
-  return `Hello! ${name} you have ${age} years of age`;
-}
+const title = document.getElementById("title");
 
-const greetNicolas = sayHello("Nicolas", 14);
-console.log(greetNicolas);
+/*
+console.log(title);
 */
 
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  multiplication: function (a, b) {
-    return a * b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  square: function (a, b) {
-    return a ** b;
-  },
-};
-
-//console.log(greetNicolas);
-const plus = calculator.plus(5, 5);
-console.log(plus);
-
-const multiplication = calculator.multiplication(3, 4);
-console.log(multiplication);
-
-const minus = calculator.minus(5, 8);
-console.log(minus);
-
-const divide = calculator.divide(8, 4);
-console.log(divide);
-
-const sqare = calculator.square(2, 4);
-console.log(sqare);
+title.innerHTML = "Hi! From JS";
