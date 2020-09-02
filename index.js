234552ab41f4103a6e53f2b8514d20a26f6c34e8
Leo-ground <68109622+Leo-ground@users.noreply.github.com>
2020-09-03 00:07:23 +0900

    #2.7

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
+
+/*
+function handleClick() {
+  const currentClass = title.className;
+  if (currentClass !== CLICKED_CLASS) {
+    title.className = CLICKED_CLASS;
+  } else {
+    title.className = "";   <---- if I create one more class in title.heml, can not react btn
+  }
+}
+*/
+
+/*
+function handleClick() {
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
+  } else {
+    title.classList.add(CLICKED_CLASS);
+  }                                               <---------too long
+}
+*/
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
-} /* color domain https://flatuicolors.com/palette/defo*/
+  title.classList.toggle(CLICKED_CLASS); //<------toggle if exist classs remove or not add
+}
 
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener("click", handleClick);
 }
+
 init();
