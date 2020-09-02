b2769247ca071c721cd712b2cfad24e055588740
Leo-ground <68109622+Leo-ground@users.noreply.github.com>
2020-09-02 15:16:27 +0900

    #1.10

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-/*const monday = "Mon";
-const tue = "Tue";
-const wed = "Wed";
-const thu = "Thu";
-const fri = "Fri";
+const nicoInfo = {
+  name: "Nico",
+  age: 33,
+  gender: "Male",
+  isHandsome: true,
+  favMovies: ["Along the gods", "LOTR", "Oldboy"],
+  favFood: [
+    {
+      name: "Kimchi",
+      fatty: false,
+    },
+    {
+      name: "Cheese burger",
+      fatty: true,
+    },
+  ],
+};
 
-console.log(monday, tue, wed, thu, fri);
+console.log(nicoInfo.favFood[0]);
+/*
+console.log(nicoInfo.gender);
+
+nicoInfo.gender = "Female";
+
+console.log(nicoInfo.gender);
 */
-
-const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-console.log(daysOfWeek[2]);
