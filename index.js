3fe14c771ab1a9ec903d3a7ee7ab1c3a05d5a3c9
Leo-ground <68109622+Leo-ground@users.noreply.github.com>
2020-09-02 16:09:07 +0900

    #2.1.1

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-function sayHello(name, age) {
-  console.log("Hello!", name, "you have", age, "years of age");
+/*function sayHello(name, age) {
+  return `Hello! ${name} you have ${age} years of age`;
 }
 
-sayHello("Nicolas", 15);
+const greetNicolas = sayHello("Nicolas", 14);
+console.log(greetNicolas);
+*/
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  multiplication: function (a, b) {
+    return a * b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  square: function (a, b) {
+    return a ** b;
+  },
+};
+
+//console.log(greetNicolas);
+const plus = calculator.plus(5, 5);
+console.log(plus);
+
+const multiplication = calculator.multiplication(3, 4);
+console.log(multiplication);
+
+const minus = calculator.minus(5, 8);
+console.log(minus);
+
+const divide = calculator.divide(8, 4);
+console.log(divide);
+
+const sqare = calculator.square(2, 4);
+console.log(sqare);
